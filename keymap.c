7b6016d165d3c3668cee53a905b10065f033726e
aomoji <41809401+aomoji@users.noreply.github.com>
2021-11-03 16:32:51 +0900

    Update keymap.c
    added expansion key

--- keymap.c
+++ keymap.c
@@ -21,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,     KC_I,    KC_O,   KC_P,  KC_BSPC,
       KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,     KC_K,    KC_L, KC_SCLN, KC_ENT,
      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,  KC_COMM,  KC_DOT, KC_SLSH, SFT_T(KC_QUOT),
-     KC_PGUP, KC_PGDN, KC_LALT, KC_LGUI,   MO(2),  LT(3, KC_SPC),  LT(3, KC_SPC),   MO(1),  KC_RGUI, KC_RALT, KC_HOME, KC_END
+     LGUI(KC_MINS), LGUI(KC_PLUS), KC_LALT, KC_LGUI,   MO(2),  LT(3, KC_SPC),  LT(3, KC_SPC),   MO(1),  KC_RGUI, KC_RALT, KC_HOME, KC_END
   ),
 
 [1] = LAYOUT(
@@ -32,10 +32,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
 [2] = LAYOUT(
-     KC_ESC,  XXXXXXX, XXXXXXX, KC_TILD, KC_PIPE, KC_LCBR, KC_RCBR,  KC_UNDS, KC_PLUS, KC_NO, KC_NO, RESET,
+     KC_ESC,  XXXXXXX, XXXXXXX, KC_TILD, KC_PIPE, KC_LCBR, KC_RCBR,  KC_UNDS, KC_PLUS, KC_NO, KC_NO, _______,
      _______, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
      _______, KC_F1,     KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,   KC_NO,
-     _______, _______, _______, _______,   MO(2), _______, _______,   MO(1), _______, _______, _______, _______
+     _______, _______, _______, _______,   MO(2), _______, _______,   MO(1), _______, _______, _______, RESET
   ),
 
 [3] = LAYOUT(
